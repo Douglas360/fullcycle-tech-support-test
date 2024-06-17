0d772c65cf7b52b214d97d17775f552302d3b287
Douglas360 <douglas_henriqueduarte@hotmail.com>
2024-06-17 14:52:01 -0300

    corrigido desafio-docker-suporte

--- desafio-docker-suporte/app/index.ts
+++ desafio-docker-suporte/app/index.ts
@@ -1,30 +1,41 @@
-import express from 'express';
+import express from "express";
 import queryDatabase from "./queryPromise";
 
 export async function createApp() {
+  const characters = [
+    "Luke Skywalker",
+    "Darth Vader",
+    "Han Solo",
+    "Chewbacca",
+    "Leia Organa",
+    "Obi-Wan Kenobi",
+    "Yoda",
+    "R2-D2",
+    "C-3PO",
+  ];
 
-    const characters = ['Luke Skywalker', 'Darth Vader', 'Han Solo', 'Chewbacca', 'Leia Organa', 'Obi-Wan Kenobi', 'Yoda', 'R2-D2', 'C-3PO']
+  const sqlInsert = "INSERT INTO `characterSW`(`name`) VALUES (?)";
 
-    const sqlInsert = 'INSERT INTO `characterSW`(`name`) VALUES (?)';
+  for (const character of characters) {
+    await queryDatabase(sqlInsert, [character]);
+  }
 
-    for (const character of characters) {
-        await queryDatabase(sqlInsert, [character]);
-    }
+  const app = express();
 
-    const app = express();
+  app.get("/", async (req, res) => {
+    const selectCharacters = "SELECT * FROM `characterSW`";
 
-    app.get('/', async (req, res) => {
-        const selectCharacters = 'SELECT * FROM `characterSW`';
+    const allCharacters = await queryDatabase(selectCharacters);
 
-        const allCharacters = await queryDatabase(selectCharacters);
-
-        const html = `<h1>Star Wars Characters</h1>\n
+    const html = `<h1>Star Wars Characters</h1>\n
   <ul>
-    ${allCharacters.map((character: {name: string}) => {
-            return `<li>${character.name}</li>`;
-        }).join('')}
-  </ul>`
-        res.send(html)
-    })
-    return app
+    ${allCharacters
+      .map((character: { name: string }) => {
+        return `<li>${character.name}</li>`;
+      })
+      .join("")}
+  </ul>`;
+    res.send(html);
+  });
+  return app;
 }
